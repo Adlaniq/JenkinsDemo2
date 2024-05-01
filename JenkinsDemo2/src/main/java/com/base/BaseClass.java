bbdaa15281f43c17829d3e6d0aa39d08f25ae8bb
Adlan Aniq <103783280+Adlaniq@users.noreply.github.com>
2024-05-02 00:31:34 +0800

    Update BaseClass.java

--- JenkinsDemo2/src/main/java/com/base/BaseClass.java
+++ JenkinsDemo2/src/main/java/com/base/BaseClass.java
@@ -2,6 +2,7 @@ package com.base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.annotations.AfterMethod;
@@ -10,28 +11,45 @@ import org.testng.annotations.BeforeMethod;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseClass {
-	WebDriver driver;
-	
-	@BeforeMethod
-	public void setup() {
-		if (System.getProperty("browser").equalsIgnoreCase("Chrome")) {
-			WebDriverManager.chromedriver().setup();
-			driver = new ChromeDriver();
-		} else if (System.getProperty("browser").equalsIgnoreCase("Firefox")) {
-			WebDriverManager.firefoxdriver().setup();
-			driver = new FirefoxDriver();
-		} else if (System.getProperty("browser").equalsIgnoreCase("IE")) {
-			WebDriverManager.iedriver().setup();
-			driver = new InternetExplorerDriver();
-		}
-		driver.manage().window().maximize();
-		driver.get(System.getProperty("url"));
-			driver.get("https://www.google.com/");
-			driver.manage().window().maximize();
-	}	
-	@AfterMethod
-	public void tearDown() {
-			driver.close();
-	}	
-
+    WebDriver driver;
+    
+    @BeforeMethod
+    public void setup() {
+        String browser = System.getProperty("browser", "Chrome").toLowerCase(); // Default to Chrome if not specified
+        
+        switch (browser) {
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                
+                // Set Chrome options including path to Chrome binary
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.setBinary("C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe");
+                driver = new ChromeDriver(chromeOptions);
+                break;
+                
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+                
+            case "ie":
+                WebDriverManager.iedriver().setup();
+                driver = new InternetExplorerDriver();
+                break;
+                
+            default:
+                throw new IllegalArgumentException("Unsupported browser: " + browser);
+        }
+        
+        driver.manage().window().maximize();
+        driver.get(System.getProperty("url", "https://www.google.com")); // Get URL from system property or default to Google
+    }
+    
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.close();
+            driver.quit(); // Ensure all sessions are closed
+        }
+    }
 }
