cfd629290141621e8871b93da3119bf447acf143
Adlan Aniq <103783280+Adlaniq@users.noreply.github.com>
2024-05-02 01:18:31 +0800

    Update BaseClass.java

--- JenkinsDemo2/src/main/java/com/base/BaseClass.java
+++ JenkinsDemo2/src/main/java/com/base/BaseClass.java
@@ -2,40 +2,36 @@ package com.base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
-public class BaseClass {
-    WebDriver driver;
-    
-    @BeforeMethod
-    public void setup() {
-        String browser = System.getProperty("browser", "Chrome").toLowerCase(); // Default to Chrome if not specified
-        
-        if (browser.equals("chrome")) {
-            // Set the path to the ChromeDriver you've downloaded
-            System.setProperty("webdriver.chrome.driver", "C:\\Users\\adlaniq\\Downloads\\chrome-headless-shell-win32\\chrome-headless-shell");
+import io.github.bonigarcia.wdm.WebDriverManager;
 
-            // Initialize Chrome options if needed
-            ChromeOptions options = new ChromeOptions();
-            // Uncomment the next line if you want to run Chrome in headless mode
-            // options.addArguments("--headless");
-            
-            driver = new ChromeDriver(options);
-        } else {
-            throw new IllegalArgumentException("Unsupported browser: " + browser);
-        }
-        
-        driver.manage().window().maximize();
-        driver.get(System.getProperty("url", "https://www.google.com")); // Get URL from system property or default to Google
-    }
-    
-    @AfterMethod
-    public void tearDown() {
-        if (driver != null) {
-            driver.close();
-            driver.quit(); // Ensure all sessions are closed
-        }
-    }
+public class BaseClass {
+	WebDriver driver;
+	
+	@BeforeMethod
+	public void setup() {
+		if (System.getProperty("browser").equalsIgnoreCase("Chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		} else if (System.getProperty("browser").equalsIgnoreCase("Firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+		} else if (System.getProperty("browser").equalsIgnoreCase("IE")) {
+			WebDriverManager.iedriver().setup();
+			driver = new InternetExplorerDriver();
+		}
+		driver.manage().window().maximize();
+		driver.get(System.getProperty("url"));
+			driver.get("https://www.google.com/");
+			driver.manage().window().maximize();
+	}	
+	@AfterMethod
+	public void tearDown() {
+			driver.close();
+	}	
+
 }
